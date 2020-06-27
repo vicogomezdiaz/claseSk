200b6efbfabf6fde64976e1a41653a2f87c63889
digitai <8643845+digitaico@users.noreply.github.com>
2020-06-27 17:27:38 +0000

    Add files via upload

--- app0/src/js/index.js
+++ app0/src/js/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const nasaApiKey = "rygwqAMdjwlsTdMA4ksrAHiyOFtqX05nlXnxZfQv";
+const nasaEndpoint = "https://api.nasa.gov/planetary/apod?api_key=";
+const nasaUrl = nasaEndpoint + nasaApiKey;
+
+
 window.addEventListener('click', (ev) => {
 
 	if(ev.target.matches('#btnMh')) { // menu hamburguesa
@@ -21,7 +26,20 @@ window.addEventListener('click', (ev) => {
 	}
 }, {capture:true, passive:true});
 
+//fetch.
+	fetch(nasaUrl)
+	.then(response => response.json())
+	.then(data => representarDatos(data));
 
+function representarDatos(data) {
+	let dataContenedor = [];
+
+		dataContenedor += `
+			<div>${data.date} - ${data.explanation}</div>
+			<div><img src="${data.url}" alt="" /></div>
+		`;
+	divData.innerHTML = dataContenedor;
+}
 
 function actMenuHam() {
 	return modalMenuHam.style.width === '175px' ? modalMenuHam.style.width = '0' : modalMenuHam.style.width = '175px'; 
@@ -43,12 +61,12 @@ function stringToUppercase(string) {
 } 
 
 
-
+// JSON: Javascript Object Notation
 let estado = [ 
 	{
 		"id": "155540",
 		"usuario":"Jeronimo Morales Ardila",
-		"status": 1,
+		"status": 0,
 		"twits":51,
 		"likes":17,
 		"foto": "/shell/img/pic/people1.png"
@@ -56,7 +74,7 @@ let estado = [
 	{
 		"id": "155541",
 		"usuario":"Daniela Ardila Lopez",
-		"status": 0,
+		"status": 1,
 		"twits":154,
 		"likes":39,
 		"foto": "/shell/img/pic/people2.png"
@@ -64,7 +82,7 @@ let estado = [
 	{
 		"id": "155542",
 		"usuario":"Santiago Hernandez",
-		"status": 0,
+		"status": 1,
 		"twits":890,
 		"likes":197,
 		"foto": "/shell/img/pic/people3.png"
@@ -99,9 +117,9 @@ estado.forEach((el, index) => {
 		<img src="/shell/img/ico/phone-call-gray.svg" class="ico" alt="" /></td>
 		`;
 	}
-		data += `
+	data += `
 		</tr>
 	`;	
-	});
+});
 
 dEstado.innerHTML = data;
