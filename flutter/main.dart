335ea2aa35665b9f5eb32f8445706b17d3ae7d0d
digitai <8643845+digitaico@users.noreply.github.com>
2020-08-08 17:15:07 +0000

    Add files via upload

--- flutter/main.dart
+++ flutter/main.dart
@@ -1,78 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-	runApp(MyApp());
+  runApp(MyApp());
 }
 
-// hot reload
 class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return MaterialApp(
-				home: Scaffold(
-						backgroundColor: Colors.white,
-						body: SafeArea(
-								child: Column (
-										crossAxisAlignment: CrossAxisAlignment.stretch,
-										children: <Widget>[
-											Container(
-													width: 100.0,
-													height:150.0,
-													color: Colors.green,
-													child: Text('primera'),
-											),
-											SizedBox(height: 5.0),
-											Container(
-													width: 90.0,
-													height: 150.0,
-													color: Colors.pink,
-													child: Row (
-															crossAxisAlignment: CrossAxisAlignment.stretch,
-															children: <Widget>[
-																Container(
-																		width: 75.0,
-																		height: 50.0,
-																		color: Colors.indigo,
-																		child: Text('c1'),
-																),
-																Container(
-																		width: 75.0,
-																		height: 50.0,
-																		color: Colors.grey,
-																		child: Text('c2'),
-																),
-																Container(
-																		width: 75.0,
-																		height: 50.0,
-																		color: Colors.grey,
-																		child: Image.network('https://static01.nyt.com/images/2013/02/10/fashion/0210Culturalstudies/0210Culturalstudies-jumbo-v2.jpg'),
-																),
-																Container(
-																		width: 75.0,
-																		height: 50.0,
-																		color: Colors.black,
-																		child: Text(
-																				'cyyyyyyyyyy4',
-																				style:TextStyle(color: Colors.white.withOpacity(0.5)),
-																		),
-																),
-															],
-															),
-															),
-															SizedBox(height: 5.0),
-
-															Container(
-																	width: 70.0,
-																	height: 150.0,
-																	color: Colors.blue,
-																	child: Text('Contenedor 003'),
-															),
-															], // Widgets list		
-															),
-															),
-															),
-															); // MaterialApp	
+			home: Scaffold(
+				backgroundColor: Colors.red ,
+				body: SafeArea(
+					child: Column(
+						crossAxisAlignment: CrossAxisAlignment.center,
+						children: <Widget>[
+							CircleAvatar(
+								radius:50.0,
+								backgroundImage: AssetImage('images/jea.jpg'),
+							),
+							Text(
+								'Jorge Eduardo Ardila',
+								style: TextStyle(
+									fontSize:25.0,
+									color: Colors.white,
+									fontFamily: 'Pacifico',
+								),
+							),
+						],	
+					),
+				),
+			), // Scaffold
+		); // MaterialApp
 	}
 }
-
 
